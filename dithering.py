5fecb909a5526157839f3b1b9adbc257e343de15
Joseph Camp <josephcamp1992@gmail.com>
2022-04-03 11:05:16 +0100

    Fix logo

--- dithering.py
+++ dithering.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from PIL import Image
 import numpy as np
 
-target_filepath = Path('toothless_full.png')
+target_filepath = Path('melvor_idle_full.png')
 img = Image.open(target_filepath).convert('RGBA')  # make sure image is RGBA
 img_3x = img.resize((img.width * 3, img.height * 3), Image.NEAREST)
 img_3x_arr = np.array(img_3x)
@@ -17,4 +17,4 @@ for i in range(img_3x_arr.shape[1]):
         img_3x_arr[:, i, 3] = 0
 
 img_3x_back = Image.fromarray(img_3x_arr)
-img_3x_back.save(f'toothless_template.png')
+img_3x_back.save(f'melvor_idle_template.png')
