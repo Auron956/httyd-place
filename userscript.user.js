3a7e06f738fe44b812f61b975bfa88e0bcdbefc3
Joseph Camp <josephcamp1992@gmail.com>
2022-04-03 14:09:24 +0100

    Attempt to work around image caching issues

--- userscript.user.js
+++ userscript.user.js
@@ -13,7 +13,8 @@ if (window.top !== window.self) {
             document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
-            i.src = "https://raw.githubusercontent.com/Auron956/httyd-place/main/melvor_idle_template.png";
+			const ts = Math.floor(Date.now() / 10000);
+            i.src = "https://raw.githubusercontent.com/Auron956/httyd-place/main/melvor_idle_template.png?tstamp=" + ts;
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 1000px;";
             console.log(i);
             return i;
